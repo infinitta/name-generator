74ab6671b4650b7625c8eb6532a33c62de40e82e
Douglas Oliveira <oliveira.douglasedu@outlook.com>
2024-01-24 00:33:31 -0300

    add typescript to project

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import { Typography } from '@mui/material';
-
-function App() {
-  return (
-    <Typography>Hello, Tellus!</Typography>
-  );
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+function App() {
+  return <Typography>Hello, Tellus!</Typography>;
+}
+
+export default App;
